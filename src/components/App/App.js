f91ebbe084c5af382c81a5f92f94b22d0e7e8339
Jeadine29 <143826069+Jeadine29@users.noreply.github.com>
2024-06-12 04:31:49 +0800

    update src

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+import user from '../../data/user.json';
+import data from '../../data/data.json';
+import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
+import { Container } from './App.styled';
+import { GlobalStyle } from 'components/GlobalStyle';
+
+export const App = () => {
+  return (
+    <Container>
+      <Profile user={user} />
+      <Statistics title="Upload stats" data={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+      <GlobalStyle />
+    </Container>
+  );
+};
